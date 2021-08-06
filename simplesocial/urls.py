8eeec3e205f8d971cb9b6bafd4c038b40550b721
M-Rb3 <rabea9220@gmail.com>
2021-08-06 14:36:23 +0200

    commit

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -19,7 +19,7 @@ from . import views
 from django.conf import settings
 from django.views.static import serve
 from django.conf.urls import url
-
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
@@ -33,4 +33,4 @@ urlpatterns = [
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
